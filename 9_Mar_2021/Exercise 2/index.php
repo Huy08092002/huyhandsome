be759fff1e3e18e84affcdfc07825903bb9b7692
huy08092002 <nguyenquanghuy12a9@gmail.com>
2021-03-12 13:37:13 +0700

    up date 9_Mar_2021 Signed-off-by: huy08092002 <nguyenquanghuy12a9@gmail.com>

---
+++ 9_Mar_2021/Exercise 2/index.php
@@ -0,0 +1,6 @@
+<?php
+    $arr=[3,5,7,9,15,50];
+    echo "Gia trị lớn nhất:".Max($arr)."</br>";
+    echo "Gia tri nho nhat:".min($arr)."</br>";
+    echo "Tinh tong trung binh cong:".(max($arr)*min($arr));
+?>
---
+++ 9_Mar_2021/Exercise 3/index.php
@@ -0,0 +1,26 @@
+<?php
+$x = 1;
+$sum = null;
+$chiahet = null;
+while ($x <= 100)
+{
+    $sum += $x;
+    $x++;
+    if($x >= 20 && $x <= 50)
+    {
+        if($x % 3==0)
+        {
+            $chiahet .= "$x ";
+        }
+    }
+}
+if($x >= 20 && $x <= 50);
+{
+    if($x % 3==0)
+    {
+        $chiahet .= "$x ";
+    }
+}
+echo " Tổng các số nguyên từ 1-100 là:".$sum."<br>";
+echo " Các số chia hết cho 3 trong khoảng từ 20-50 là:".$chiahet;
+?>
